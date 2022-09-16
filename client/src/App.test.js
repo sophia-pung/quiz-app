3116531e8ff4baa894bec556b4a3d0dae128f4ad
sophia pung <sophiagracepung@gmail.com>
2022-09-16 15:14:08 -0700

    adding a test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,18 +3,6 @@ import App from "./App";
 import Header from "./components/header.js";
 import UserForm from "./components/user";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-// First Test - Check if Welcome to my game is present:
-test("Welcome to my game", () => {
-  render(<Header />);
-  expect(screen.getByText(/Welcome to the game:/));
-});
-
 //Second Test - Check whether the placeholder text works.
 test("check if email element is in form", () => {
   render(<UserForm />);
